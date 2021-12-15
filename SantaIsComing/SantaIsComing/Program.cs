d06a7e2f4a8aaabbca39fe4106e568ee79c9cdbd
Opilane TTHK <opilane@tthk.ee>
2021-12-15 15:04:12 +0200

    first commit

---
+++ SantaIsComing/SantaIsComing/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SantaIsComing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
